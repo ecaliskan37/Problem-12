a621ae0cdc4117b96157a2b5995d8da08b4a0509
ecaliskan37 <ebru-1-mat@hotmail.com>
2025-07-22 13:14:02 +0300

    fff

--- src/components/Hesabım.jsx
+++ src/components/Hesabım.jsx
@@ -1,4 +1,36 @@
 const Hesabım = () => {
-  return <p>Ad-soyad</p>
+  return (
+    <>
+      <form>
+        <div className="mb-4">
+          <label>Ad1 Soyad1 </label>
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Username
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="text"
+              placeholder="Kullanıcı Adı"
+            />
+          </label>
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Password
+            <input
+              type="password"
+              className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="******"
+            />
+          </label>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+            Change
+          </button>
+        </div>
+      </form>
+      <p class="text-center text-gray-500 text-xs">
+        &copy;2020 Acme Corp. All rights reserved.
+      </p>
+    </>
+  )
 }
 export default Hesabım
